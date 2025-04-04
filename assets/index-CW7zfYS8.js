1f2f7c553f113c9f74d7d888e6e69d67f0ee2e76
jenkins-admin <xxxx@gmail.com>
2025-04-04 22:19:49 +0900

    Build #68

---
+++ assets/index-CW7zfYS8.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-lj_M-6By.js";import"./vendor-DP-u-qT_.js";const s=()=>t.jsx("div",{children:t.jsx("h1",{children:"About"})});export{s as default};
---
+++ assets/index-Dn3MUUbn.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-lj_M-6By.js";import"./vendor-DP-u-qT_.js";const n=()=>t.jsx("div",{children:t.jsx("h1",{children:"Contact"})});export{n as default};
